e97537e08de11c1381521ebf4a5c218b801b4c1b
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-01-07 15:54:32 +0100

    Withdrawal done method done, changed testname to Jari in main

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -22,32 +22,47 @@ public class Main {
         TransactionManagement management = context.getBean(TransactionManagementImpl.class);
 
 
-        Wallet simon_wallet = walletManagement.create("Simon's wallet");
+        Wallet jari_wallet = walletManagement.create("Jari's wallet");
 
 
-        Transaction transactionBTC1Simon = management.createDepositTransaction(
-                simon_wallet.getId(),
+        Transaction transactionBTC1Jari = management.createDepositTransaction(
+                jari_wallet.getId(),
                 CryptoCurrency.BTC,
                 new BigDecimal(1),
                 "Test"
         );
 
-        Transaction transactionETH10Simon = management.createDepositTransaction(
-                simon_wallet.getId(),
+        Transaction transactionETH10Jari = management.createDepositTransaction(
+                jari_wallet.getId(),
                 CryptoCurrency.ETH,
                 new BigDecimal(10),
                 "test"
         );
 
-        Transaction transactionBNB1000Simon = management.createDepositTransaction(
-                simon_wallet.getId(),
+        Transaction transactionBNB1000Jari = management.createDepositTransaction(
+                jari_wallet.getId(),
                 CryptoCurrency.BNB,
                 new BigDecimal(1000),
                 "test"
         );
 
-        System.out.println(walletManagement.getById(simon_wallet.getId()));
+        System.out.println(walletManagement.getById(jari_wallet.getId()));
+        System.out.println("-----------------------------------------");
 
-
+        /* //Working as intended
+        Transaction shouldFail = management.createWithdrawalTransaction(
+                jari_wallet.getId(),
+                CryptoCurrency.BTC,
+                new BigDecimal(2),
+                "More than in bank should throw exception"
+        );
+         */
+        Transaction transactionWithdrawalBNBJari = management.createWithdrawalTransaction(
+                jari_wallet.getId(),
+                CryptoCurrency.BNB,
+                new BigDecimal(2),
+                "More than in bank should throw exception"
+        );
+        System.out.println(walletManagement.getById(jari_wallet.getId()));
     }
 }
